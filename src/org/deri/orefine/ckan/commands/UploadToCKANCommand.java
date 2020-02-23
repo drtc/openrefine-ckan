0eb96c6bde89dfc3389beb72b07a541b2896d2af
Saiful Amin <saiful@semanticconsulting.com>
2020-02-23 23:18:21 +0530

    code reformat for idea

--- src/org/deri/orefine/ckan/commands/UploadToCKANCommand.java
+++ src/org/deri/orefine/ckan/commands/UploadToCKANCommand.java
@@ -27,78 +27,78 @@ import com.google.refine.exporters.ExporterRegistry;
 import com.google.refine.model.Project;
 
 
-public class UploadToCKANCommand extends Command{
+public class UploadToCKANCommand extends Command {
 
-        private static final String CKAN_DEFAULT_BASE_URL = "http://master.ckan.org/";
+    private static final String CKAN_DEFAULT_BASE_URL = "http://master.ckan.org/";
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		Project project = getProject(request);
-		String apikey = request.getParameter("apikey");
-		String ckanApiBase = request.getParameter("ckan_base_api");
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Project project = getProject(request);
+        String apikey = request.getParameter("apikey");
+        String ckanApiBase = request.getParameter("ckan_base_api");
 
-                Slugify slg = new Slugify();
-		final String packageName = request.getParameter("package_id");
-                final String packageId = slg.slugify(packageName);
+        Slugify slg = new Slugify();
+        final String packageName = request.getParameter("package_id");
+        final String packageId = slg.slugify(packageName);
 
-		boolean createNewIfNonExisitng = getBoolean(request,"create_new");
-		boolean addProvenanceInfo = false;
-		boolean rememberApiKey = getBoolean(request,"remember_api_key");
+        boolean createNewIfNonExisitng = getBoolean(request, "create_new");
+        boolean addProvenanceInfo = false;
+        boolean rememberApiKey = getBoolean(request, "remember_api_key");
 
-		//get (comma-separated list of ) formats required to be uploaded
-		String files = request.getParameter("files");
+        //get (comma-separated list of ) formats required to be uploaded
+        String files = request.getParameter("files");
 
-		try{
+        try {
 
-		        JSONObject options = new JSONObject(request.getParameter("options"));
-			if(ckanApiBase==null || ckanApiBase.isEmpty()){
-				throw new RuntimeException("Some required parameters are missing: CKAN API base URL");
-			}
-			if(packageId==null || packageId.isEmpty()){
-				throw new RuntimeException("Some required parameters are missing: package ID");
-			}
-			if(rememberApiKey){
-				saveApiKey(apikey);
-				saveCKANBase(ckanApiBase);
-			}else{
-				forgetApiKey();
-				forgetCKANBase();
-			}
-			//remove the last "/" from CKAN API base if it exists
-			ckanApiBase = ckanApiBase.trim();
-			if(ckanApiBase.endsWith("/")){
-				ckanApiBase = ckanApiBase.substring(0, ckanApiBase.length()-1);
-			}
+            JSONObject options = new JSONObject(request.getParameter("options"));
+            if (ckanApiBase == null || ckanApiBase.isEmpty()) {
+                throw new RuntimeException("Some required parameters are missing: CKAN API base URL");
+            }
+            if (packageId == null || packageId.isEmpty()) {
+                throw new RuntimeException("Some required parameters are missing: package ID");
+            }
+            if (rememberApiKey) {
+                saveApiKey(apikey);
+                saveCKANBase(ckanApiBase);
+            } else {
+                forgetApiKey();
+                forgetCKANBase();
+            }
+            //remove the last "/" from CKAN API base if it exists
+            ckanApiBase = ckanApiBase.trim();
+            if (ckanApiBase.endsWith("/")) {
+                ckanApiBase = ckanApiBase.substring(0, ckanApiBase.length() - 1);
+            }
 
-			CkanApiProxy ckanApiClient = new CkanApiProxy();
+            CkanApiProxy ckanApiClient = new CkanApiProxy();
 
-			Engine engine = getEngine(request, project);
-			StringTokenizer tokenizer = new StringTokenizer(files, ",");
-			Set<Exporter> exporters = new HashSet<Exporter>();
-			while(tokenizer.hasMoreTokens()){
-				String format = tokenizer.nextToken();
-				Exporter exporter = ExporterRegistry.getExporter(format);
+            Engine engine = getEngine(request, project);
+            StringTokenizer tokenizer = new StringTokenizer(files, ",");
+            Set<Exporter> exporters = new HashSet<Exporter>();
+            while (tokenizer.hasMoreTokens()) {
+                String format = tokenizer.nextToken();
+                Exporter exporter = ExporterRegistry.getExporter(format);
 
-				if(exporter==null){
-					//handle the specialcase of provenance
-					if(format.equals("add_provenance_info")){
-						addProvenanceInfo = true;
-						continue;
-					}
-					//either JSON representation of the history or something went wrong
-					if(format.equals("history-json")){
-						exporter = new HistoryJsonExporter();
-					}else{
-						//fail
-						respondException(response, new RuntimeException("Unknown exporter format"));
-						return;
-					}
-				}
-				exporters.add(exporter);
-			}
+                if (exporter == null) {
+                    //handle the specialcase of provenance
+                    if (format.equals("add_provenance_info")) {
+                        addProvenanceInfo = true;
+                        continue;
+                    }
+                    //either JSON representation of the history or something went wrong
+                    if (format.equals("history-json")) {
+                        exporter = new HistoryJsonExporter();
+                    } else {
+                        //fail
+                        respondException(response, new RuntimeException("Unknown exporter format"));
+                        return;
+                    }
+                }
+                exporters.add(exporter);
+            }
 
-			final String packageUrl = ckanApiClient.addGroupOfResources(ckanApiBase , packageName, options, exporters, project, engine,
-					new ProvenanceFactory(), apikey, createNewIfNonExisitng, addProvenanceInfo);
+            final String packageUrl = ckanApiClient.addGroupOfResources(ckanApiBase, packageName, options, exporters, project, engine,
+                    new ProvenanceFactory(), apikey, createNewIfNonExisitng, addProvenanceInfo);
 //			TODO: Move to Jackson data-bind from org.json
 //			respondJSON(response, new Jsonizable() {
 //
@@ -115,28 +115,28 @@ public class UploadToCKANCommand extends Command{
 //					writer.endObject();
 //				}
 //			});
-		}catch(Exception e){
-		    respondException(response, e);
-		}
-	}
-
-	private void saveApiKey(String key){
-		ProjectManager.singleton.getPreferenceStore().put("CKAN.api_key", key);
-	}
-
-	private void forgetApiKey(){
-		ProjectManager.singleton.getPreferenceStore().put("CKAN.api_key", "");
-	}
-
-	private void saveCKANBase(String key){
-            ProjectManager.singleton.getPreferenceStore().put("CKAN.ckan_base_url", key);
+        } catch (Exception e) {
+            respondException(response, e);
         }
+    }
 
-        private void forgetCKANBase(){
-            ProjectManager.singleton.getPreferenceStore().put("CKAN.ckan_base_url", CKAN_DEFAULT_BASE_URL);
-        }
+    private void saveApiKey(String key) {
+        ProjectManager.singleton.getPreferenceStore().put("CKAN.api_key", key);
+    }
 
-	private boolean getBoolean(HttpServletRequest request, String paramName){
-		return request.getParameter(paramName)!=null && request.getParameter(paramName).toLowerCase().equals("true");
-	}
+    private void forgetApiKey() {
+        ProjectManager.singleton.getPreferenceStore().put("CKAN.api_key", "");
+    }
+
+    private void saveCKANBase(String key) {
+        ProjectManager.singleton.getPreferenceStore().put("CKAN.ckan_base_url", key);
+    }
+
+    private void forgetCKANBase() {
+        ProjectManager.singleton.getPreferenceStore().put("CKAN.ckan_base_url", CKAN_DEFAULT_BASE_URL);
+    }
+
+    private boolean getBoolean(HttpServletRequest request, String paramName) {
+        return request.getParameter(paramName) != null && request.getParameter(paramName).toLowerCase().equals("true");
+    }
 }
